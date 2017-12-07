c18077bbac58a967ad907fd4bbc104b37357d63a
Ychergui <yacinechergui.pro@gmail.com>
2017-12-07 18:37:26 +0100

    Add project files.

---
+++ StatusBar/StatusBar.Android/SplashScreenActivity.cs
@@ -0,0 +1,23 @@
+using Android.App;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using StatusBar.Droid;
+
+namespace StatusBar.Droid
+{
+    [Activity(Label = "StatusBar.Android", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, Theme = "@style/StatusBar.SplashScreen")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetStatusBarColor(Color.ParseColor("#0D4680"));
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
